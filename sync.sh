700ccb0eeed3d7186c3abaef934c083f819483b7
Drvulcanlord <122398995+Eli-Watson@users.noreply.github.com>
2024-07-27 21:40:32 -0500

    add balisong

--- sync.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Define paths
-source_folder="Files/resources/Web_Content"
-destination_folder="/content"
-
-# Sync changes
-rsync -av --delete "$source_folder/" "$destination_folder/"
